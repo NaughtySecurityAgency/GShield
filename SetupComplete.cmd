769c45b03adb4e2459992d94bcb0322154764cd3
NaughtySecurityAgency <154551584+NaughtySecurityAgency@users.noreply.github.com>
2024-01-07 08:08:56 +0100

    Update SetupComplete.cmd

--- SetupComplete.cmd
+++ SetupComplete.cmd
@@ -9,6 +9,14 @@ pushd %~dp0
 cd Bin
 setlocal enabledelayedexpansion
 
+:: Create restore point
+for /f "delims=" %%a in ('wmic OS Get localdatetime ^| find "."') do set datetime=%%a
+set datetime=!datetime:~0,14!
+set datetime=!datetime:~0,4!-!datetime:~4,2!-!datetime:~6,2! !datetime:~8,2!:!datetime:~10,2!:!datetime:~12,2!
+set restorePointDescription=AutoRestorePoint_!datetime!
+wmic /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "%restorePointDescription%", 100, 7
+echo Restore point created: %restorePointDescription%
+
 :: Powershell
     for %%A in (*.ps1) do (
         @powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%%A" -WindowStyle Hidden
